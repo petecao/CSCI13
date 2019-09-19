897a9b9f2975fcbd52ee6ff003781d7ced6e5a95
thegreennapalm <peterspellingbee@gmail.com>
2019-09-18 20:35:50 -0700

    week 4

--- Lab2/src/test/java/src/LineTest.java
+++ Lab2/src/test/java/src/LineTest.java
@@ -78,7 +78,7 @@ public class LineTest {
     }
 
     /**
-     * Testing iterator() method
+     * Testing iterator.hasNext() method on an empty line
      */
     @Test
     public void iteratorEmpty() {
@@ -89,7 +89,7 @@ public class LineTest {
     }
 
     /**
-     * Testing iterator() method
+     * Testing iterator.hasNext() method with one point
      */
     @Test
     public void iteratorOne() {
@@ -102,7 +102,22 @@ public class LineTest {
     }
 
     /**
-     * Testing iterator() method
+     * Testing iterator.hasNext() method at the end of iteration
+     */
+    @Test
+    public void iteratorEndofList() {
+        Line line = new Line();
+        Point p = new Point();
+        line.add(p);
+        Iterator iterator = line.iterator();
+        assertEquals("iterator next", p,  iterator.next());
+        assertFalse("no more items", iterator.hasNext());
+
+
+    }
+
+    /**
+     * Testing iterator() method with 1 point
      */
     @Test
     public void iteratorNext() {
@@ -115,16 +130,31 @@ public class LineTest {
     }
 
     /**
-     * Testing iterator() method
+     * Testing iterator.next() method with 6 points
      */
     @Test
     public void iterator() {
         Line line = new Line();
-        line.add(new Point(1.5,2.5));
-        line.add(new Point(0.0,2.5));
-        line.add(new Point(0.0,0.0));
-        line.add(new Point(3.0,4.0));
-        line.add(new Point(8.0,-8.0));
-        line.add(new Point(-8,-8));
+        Point point1 = new Point(1.5,2.5);
+        Point point2 = new Point(0.0,2.5);
+        Point point3 = new Point(0.0,0.0);
+        Point point4 = new Point(3.0,4.0);
+        Point point5 = new Point(8.0,-8.0);
+        Point point6 = new Point(-8,-8);
+        line.add(point1);
+        line.add(point2);
+        line.add(point3);
+        line.add(point4);
+        line.add(point5);
+        line.add(point6);
+        Iterator iterator = line.iterator();
+        assertEquals("iterator next", point1,  iterator.next());
+        assertEquals("iterator next", point2,  iterator.next());
+        assertEquals("iterator next", point3,  iterator.next());
+        assertEquals("iterator next", point4,  iterator.next());
+        assertEquals("iterator next", point5,  iterator.next());
+        assertEquals("iterator next", point6,  iterator.next());
+
+
     }
 }
