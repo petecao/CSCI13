e1441de9633184b1a0df0bfbf1c9c33bbb0dd72d
thegreennapalm <peterspellingbee@gmail.com>
2019-10-02 19:38:10 -0700

    Update Postfix.java

--- Infix/src/main/java/infix/Postfix.java
+++ Infix/src/main/java/infix/Postfix.java
@@ -1,4 +1,9 @@
-package infix;
+package postfix;
+
+import java.nio.channels.ConnectionPendingException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 /**
  * Postfix evaluator
@@ -12,7 +17,59 @@ public class Postfix {
         // empty constructor
     }
 
+    /**
+     * @param expression postfix String
+     * @return result of expression
+     */
     public static double eval(String expression) {
-        return 0.0;
+
+        Deque<Double> operandStack = new ArrayDeque<>();
+
+        StringTokenizer postfixParser = new StringTokenizer(expression, "+-*/^() ", true);
+
+
+        /*
+         * Process the tokens.
+         */
+        while (postfixParser.hasMoreTokens()) {
+
+            /*
+             * Get the next token and get the first character of the token
+             */
+            String token = postfixParser.nextToken().trim();
+
+            /*
+             * If we have a token after trimming off all the white space then
+             * evaluate
+             */
+            if (token.length() > 0) {
+                switch (token) {
+                    case "+":
+                        double rhsAdd = operandStack.pop();
+                        double lhsAdd = operandStack.pop();
+                        operandStack.push(lhsAdd + rhsAdd);
+                        break;
+                    case "-":
+                        double rhsSubtract = operandStack.pop();
+                        double lhsSubtract = operandStack.pop();
+                        operandStack.push(lhsSubtract - rhsSubtract);
+                        break;
+                    case "*":
+                        double rhsMultiply = operandStack.pop();
+                        double lhsMultiply = operandStack.pop();
+                        operandStack.push(lhsMultiply * rhsMultiply);
+                        break;
+                    case "/":
+                        double rhsDivide = operandStack.pop();
+                        double lhsDivide = operandStack.pop();
+                        operandStack.push(lhsDivide / rhsDivide);
+                        break;
+                    default:
+                        operandStack.push(Double.valueOf(token));
+                        break;
+                }
+            }
+        }
+        return operandStack.pop();
     }
 }
